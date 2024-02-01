ffb258f71c45072525202957112ddab003e77cac
Ren <42751478+REN-aissance@users.noreply.github.com>
2024-02-01 22:56:12 +0900

    Refactor, fixed text bug

--- src/chip8.rs
+++ src/chip8.rs
@@ -20,6 +20,7 @@ use std::time::Duration;
 pub enum CPUError {
     UnknownOpcode(u16, u16),
     RamOutOfBounds,
+    InvalidTextIndex(u8),
 }
 
 impl fmt::Display for CPUError {
@@ -30,6 +31,7 @@ impl fmt::Display for CPUError {
                 i, pc
             ),
             CPUError::RamOutOfBounds => write!(f, "ERROR: Ran out of RAM"),
+            CPUError::InvalidTextIndex(vx) => write!(f, "ERROR: Invalid text index 0x{:0X}", vx),
         }
     }
 }
@@ -100,29 +102,29 @@ impl Chip8 {
         }
     }
 
-    fn execute_instruction(&mut self, i: u16) -> Result<Chip8Event, Error> {
+    fn execute_instruction(&mut self, op: u16) -> Result<Chip8Event, Error> {
         #[cfg(feature = "trace")]
         println!("{:?}", self);
-        let [ub, lb] = i.to_be_bytes();
+        let [ub, lb] = op.to_be_bytes();
         let x = (ub & 0x0F) as usize;
         let y = (lb >> 4) as usize;
         let n = lb & 0x0F;
         let vx = *self.reg.get(x).unwrap();
         let vy = *self.reg.get(y).unwrap();
-        match i {
+        match op {
             //00E0 CLS
             0x00E0 => self.screen.clear(),
             //00EE RET
             0x00EE => self.pc = self.stack.pop(),
             //1nnn JP addr
             0x1000..=0x1FFF => {
-                self.pc = i & 0x0FFF;
+                self.pc = op & 0x0FFF;
                 return Ok(Chip8Event::DoNotIncrementPC);
             }
             //2nnn CALL addr
             0x2000..=0x2FFF => {
                 self.stack.push(self.pc);
-                self.pc = i & 0x0FFF;
+                self.pc = op & 0x0FFF;
                 return Ok(Chip8Event::DoNotIncrementPC);
             }
             //3nnn SE Vx, byte
@@ -138,7 +140,7 @@ impl Chip8 {
                 }
             }
             //5xy0 SE
-            0x5000..=0x5FF0 if i & 0x000F == 0 => {
+            0x5000..=0x5FF0 if op & 0xF == 0 => {
                 if vx == vy {
                     return Ok(Chip8Event::SkipNextInstruction);
                 }
@@ -196,19 +198,19 @@ impl Chip8 {
                     self.reg[x] = vy << 1;
                     self.reg[0xF] = out_bit;
                 }
-                _ => return Err(CPUError::UnknownOpcode(i, self.pc).into()),
+                _ => return Err(CPUError::UnknownOpcode(op, self.pc).into()),
             },
             //9xy0 SNE Vx, Vy
-            0x9000..=0x9FF0 if i & 0xF == 0 => {
+            0x9000..=0x9FF0 if op & 0xF == 0 => {
                 if vx != vy {
                     return Ok(Chip8Event::SkipNextInstruction);
                 }
             }
             //Annn LD I, addr
-            0xA000..=0xAFFF => self.i = i & 0x0FFF,
+            0xA000..=0xAFFF => self.i = op & 0x0FFF,
             //Bnnn JP V0, addr
             0xB000..=0xBFFF => {
-                self.pc = (i & 0x0FFF) + self.reg[0x0] as u16;
+                self.pc = (op & 0x0FFF) + self.reg[0x0] as u16;
                 return Ok(Chip8Event::DoNotIncrementPC);
             }
             //Cnnn RND Vx, byte
@@ -227,16 +229,16 @@ impl Chip8 {
             0xE000..=0xEFFF => match lb {
                 //Ex9E SKP Vx
                 0x9E => {
-                    // #[cfg(feature = "kb_debug")]
-                    // println!("Checking for key press {:X}", x);
+                    #[cfg(feature = "kb_debug")]
+                    println!("Checking for key press {:X}", x);
                     if self.kb.is_pressed(vx as usize) {
                         return Ok(Chip8Event::SkipNextInstruction);
                     };
                 }
                 //ExA1 SKNP Vx
                 0xA1 => {
-                    // #[cfg(feature = "kb_debug")]
-                    // println!("Checking key not pressed {:X}", x);
+                    #[cfg(feature = "kb_debug")]
+                    println!("Checking key not pressed {:X}", x);
                     if !self.kb.is_pressed(vx as usize) {
                         return Ok(Chip8Event::SkipNextInstruction);
                     };
@@ -265,7 +267,13 @@ impl Chip8 {
                 //Fx1E ADD I, Vx
                 0x1E => self.i = self.i.wrapping_add(vx as u16),
                 //Fx29 LD F, Vx
-                0x29 => self.i = x as u16 * 5,
+                0x29 => {
+                    if vx <= 0xF {
+                        self.i = vx as u16 * 5
+                    } else {
+                        return Err(CPUError::InvalidTextIndex(vx).into());
+                    }
+                }
                 //Fx33 LD B, Vx
                 0x33 => {
                     let i = self.i as usize;
@@ -287,6 +295,7 @@ impl Chip8 {
                             self.ram[self.i as usize + i] = vx;
                         });
                     self.i += (x + 1) as u16;
+                    dbg!(&self.ram[0x500..=0x0503]);
                 }
                 //Fx65 LD Vx, [I]
                 0x65 => {
@@ -299,12 +308,12 @@ impl Chip8 {
                         });
                     self.i += (x + 1) as u16;
                 }
-                _ => return Err(CPUError::UnknownOpcode(i, self.pc).into()),
+                _ => return Err(CPUError::UnknownOpcode(op, self.pc).into()),
             },
-            //Treat blank memory as NOP
+            // Treat blank memory as NOP
             0x0000 => return Ok(Chip8Event::DoNotIncrementPC),
             _ => {
-                return Err(CPUError::UnknownOpcode(i, self.pc).into());
+                return Err(CPUError::UnknownOpcode(op, self.pc).into());
             }
         }
         Ok(Chip8Event::IncrementPC)
@@ -317,8 +326,8 @@ impl Chip8 {
             if self.kb_halt_reg.is_some() {
                 return None;
             }
-            let i = b1 << 8 | b2;
-            match self.execute_instruction(i) {
+            let op = b1 << 8 | b2;
+            match self.execute_instruction(op) {
                 Ok(e) => match e {
                     Chip8Event::SkipNextInstruction => {
                         self.increment_pc();
